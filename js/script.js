1cc39bffd4e68a562f2de897710cefc65dd5e6e1
Piotr Przechlewski <haesor@gmail.com>
2018-01-28 18:48:48 +0100

    add script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = 5
+var h = 6
+var triangleArea = a*h/2
+var area = Math.sqrt(a*h/2)
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
